ae6ab042af811740f8adedd95ca37e1be85a5703
André Tonelli <andreveigatonelli@gmail.com>
2020-12-13 15:03:23 +0000

    Fixing code to fit tests #1

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -31,7 +31,7 @@ describe('controllers', function() {
         request(server)
           .get('/hello')
           .query({
-            name: 'Scott'
+            name: 'Joe'
           })
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
